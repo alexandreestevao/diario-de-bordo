4fada5497c55cdc968cab563de59365252826a19
Alexandre Estevao <alexandresestevao@gmail.com>
2020-09-18 09:04:27 -0300

    Created project

---
+++ backend/src/test/java/com/com/zalfsistemas/diario/DiarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.zalfsistemas.diario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
